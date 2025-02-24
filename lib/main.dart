db519703614efe5d3673bde5539e6013529f6fe0
tan8325 <tbach4@student.gsu.edu>
2025-02-24 13:26:29 -0500

    Modified code to do age counter w/ messages and colors

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
   setupWindow();
   runApp(
     ChangeNotifierProvider(
-      create: (context) => Counter(),
+      create: (context) => AgeCounter(),
       child: const MyApp(),
     ),
   );
@@ -20,7 +20,7 @@ const double windowHeight = 640;
 void setupWindow() {
   if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
     WidgetsFlutterBinding.ensureInitialized();
-    setWindowTitle('Provider Counter');
+    setWindowTitle('ACT06');
     setWindowMinSize(const Size(windowWidth, windowHeight));
     setWindowMaxSize(const Size(windowWidth, windowHeight));
     getCurrentScreen().then((screen) {
@@ -33,12 +33,33 @@ void setupWindow() {
   }
 }
 
-class Counter with ChangeNotifier {
-  int value = 0;
+class AgeCounter with ChangeNotifier {
+  int age = 0;
 
   void increment() {
-    value += 1;
+    age++;
     notifyListeners();
+  }
+
+  void decrement() {
+    if (age > 0) {
+      age--;
+      notifyListeners();
+    }
+  }
+
+  Map<String, dynamic> get milestone {
+    if (age <= 12) {
+      return {"message": "You're a child!", "color": Colors.lightBlue};
+    } else if (age <= 19) {
+      return {"message": "Teenager time!", "color": Colors.lightGreen};
+    } else if (age <= 30) {
+      return {"message": "You're a young adult!", "color": Colors.yellow.shade200};
+    } else if (age <= 50) {
+      return {"message": "You're an adult now!", "color": Colors.orange};
+    } else {
+      return {"message": "Golden years!", "color": Colors.grey};
+    }
   }
 }
 
@@ -48,7 +69,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ACT06',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
@@ -63,29 +84,46 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ageCounter = Provider.of<AgeCounter>(context);
+
     return Scaffold(
+      backgroundColor: ageCounter.milestone["color"],
       appBar: AppBar(
-        title: const Text('Flutter Demo Home Page'),
+        title: const Text('ACT06'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Consumer<Counter>(
-              builder: (context, counter, child) => Text(
-                '${counter.value}',
+            const Text('Your current age:'),
+            Consumer<AgeCounter>(
+              builder: (context, ageCounter, child) => Text(
+                '${ageCounter.age}',
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
             ),
+            const SizedBox(height: 20),
+            Text(
+              ageCounter.milestone["message"],
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: ageCounter.decrement,
+                  child: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 20),
+                FloatingActionButton(
+                  onPressed: ageCounter.increment,
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Provider.of<Counter>(context, listen: false).increment();
-        },
-        child: const Icon(Icons.add),
       ),
     );
   }
